407fa7b35acbeccaca80486ab233cc786c36851f
Programmer-Rakes <rakeshaps8866@gmail.com>
2024-07-17 02:11:51 +0530

    basics of using variables

--- tut4.cpp
+++ tut4.cpp
@@ -10,7 +10,7 @@ int main(){
     bool is_true = true;
     cout <<"This is tutorial 4. \nHere the value of a is "<<a<<". \nThe value of b is "<< b;
     cout << "\nThe value of pi is :"<< pi;
-    cout << "\nThe value of c is : "<< c;
+    cout << "\nThe value of c is : "<< c;         // "<<" is called Insertion operator.
     cout << is_true;         //if statement is true terminal will print "1"
     return 0;
 }
