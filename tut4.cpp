cf95745d1375c63c91ebeec97d064bfcffc9cf88
Programmer-Rakes <rakeshaps8866@gmail.com>
2024-07-16 21:27:12 +0530

    just added

---
+++ tut4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 4, b = 5;
+    float pi = 3.14;
+    char c = 'd';
+    bool is_true = true;
+    cout <<"This is tutorial 4. \nHere the value of a is "<<a<<". \nThe value of b is "<< b;
+    cout << "\nThe value of pi is :"<< pi;
+    cout << "\nThe value of c is : "<< c;
+    cout << is_true;         //if statement is true terminal will print "1"
+    return 0;
+}
