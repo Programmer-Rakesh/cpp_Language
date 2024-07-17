1bdb8d871ad60725604da31c72ead53b00d5504c
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-07-18 00:45:13 +0530

    New File

--- tut5.cpp
+++ tut5.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    int num1,  num2;
+    cout << "Entre the value of num1 : ";    // "<<" is called Insertion operator.
+    cin >> num1;                              // ">>" is called Extraction operator.
+
+    cout << "Entre the value of num2 : ";
+    cin >> num2;
+
+    cout << "The sum is "<< num1 + num2;
+
+    return 0;
 }
