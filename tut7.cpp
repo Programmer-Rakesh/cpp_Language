b8c17fa65493666766370cc43c732c35cd9df3da
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-08-05 21:35:30 +0530

    New files

--- tut7.cpp
+++ tut7.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int c = 45;           // Global variable
 
 int main(){           // Local variable
+// ****************Build in Dta types****************
     int a, b, c;
-    float d = 34.5;
-    long double e = 34.4;
+
+    float d = 34.5f;                   // "f" means float
+    long double e = 64.4l;             // "l" means long double
+
     cout<<"Entre the value of a : ";
     cin>>a;
     cout<<"Entre the value of b : ";
@@ -16,7 +19,23 @@ int main(){           // Local variable
     c = a + b;
     cout<<"The sum is "<<c<<endl;
     cout<<"The global c is "<<::c<<endl;     // This line is for bringing the global c inside local variable.
-    cout<<"The value of d is : "<<d<<endl<<"The value of e is : "<<e;
+    cout<<"The value of d is : "<<d<<endl<<"The value of e is : "<<e<<endl;
+
+// ***************Float, double and long double Literals****************
+    
+    cout<<"The size of 34.4 is "<<sizeof(34.4)<<endl;   
+    cout<<"The size of 34.4 is "<<sizeof(34.4f)<<endl;   
+    cout<<"The size of 34.4 is "<<sizeof(34.4F)<<endl;   
+    cout<<"The size of 34.4 is "<<sizeof(34.4l)<<endl;   
+    cout<<"The size of 34.4 is "<<sizeof(34.4L)<<endl;  
+    cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e; 
+
+// **************Reference Variables***************
+
+    float x = 455;
+    float & y = x;
+    cout<<x<<endl;
+    cout<<y<<endl;
 
     return 0;
 }
