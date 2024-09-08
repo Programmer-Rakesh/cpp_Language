29853376693c7514fecbc922ee32fb129f59b3c4
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-08 12:48:58 +0530

    New File

---
+++ tut10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /* Loops in c++ :
+    There are thre types of loops in c++ :
+        1. For loop
+        2. While loop
+        3. do-While loop
+    */
+   // For loop in c++ :
+
+   for (int i = 0; i < 40; i++)
+   {
+    cout<<i<<endl;
+   }
+   
+   return 0;
+}
