2a422557b4c73cd03c4ba9c3f2eb0e9e8e95dbcc
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-09 14:17:56 +0530

    New Files

--- tut10.cpp
+++ tut10.cpp
@@ -9,11 +9,52 @@ int main(){
         3. do-While loop
     */
    // For loop in c++ :
+   // int i=1
+   // cout<<i;
+   // i++;
+
+
+   // Syntax for for-loop:
+   // for (initialization; condition; updation)
+   // {
+   //     loop body(c++ code);
+   // }
+
 
    for (int i = 0; i < 40; i++)
    {
     cout<<i<<endl;
    }
-   
+
+
+   /* Example for infinite for-loop:
+   for (int i = 1; 34 <= 40; i++)
+   {
+     cout<<i<<enbdl;
+   }
+   */
+
+  /* While loop in c++
+  // Syntax:
+  while(condition):
+  {
+    c++ statement;
+  }
+  */
+
+ // Example of infinite while-loop:
+ int i=1;
+ while(true){
+    cout<<i<<endl;
+    i++;
+ }
+
+// Printing 1 to 40 using while loop:
+int i=1;
+while(i<=40){
+    cout<<i<<endl;
+    i++;
+}  
+
    return 0;
 }
