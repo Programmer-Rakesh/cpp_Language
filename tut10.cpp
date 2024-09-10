b8fc04a8d1f6e36b03d0586535e65eb14436dd3a
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-10 20:04:43 +0530

    New File

--- tut10.cpp
+++ tut10.cpp
@@ -2,43 +2,42 @@
 using namespace std;
 
 int main(){
-    /* Loops in c++ :
-    There are thre types of loops in c++ :
-        1. For loop
-        2. While loop
-        3. do-While loop
-    */
-   // For loop in c++ :
-   // int i=1
-   // cout<<i;
-   // i++;
+  /* Loops in c++ :
+  There are thre types of loops in c++ :
+    1. For loop
+    2. While loop
+    3. do-While loop
+  */
+  // For loop in c++ :
+  // int i=1
+  // cout<<i;
+  // i++;
 
 
-   // Syntax for for-loop:
-   // for (initialization; condition; updation)
-   // {
-   //     loop body(c++ code);
-   // }
+  // Syntax for for-loop:
+  // for (initialization; condition; updation)
+  // {
+  //     loop body(c++ code);
+  // }
 
 
-   for (int i = 0; i < 40; i++)
-   {
-    cout<<i<<endl;
-   }
+  for (int i = 0; i < 40; i++)
+  {
+  cout<<i<<endl;
+  }
 
-
-   /* Example for infinite for-loop:
-   for (int i = 1; 34 <= 40; i++)
-   {
-     cout<<i<<enbdl;
-   }
-   */
+  /* Example for infinite for-loop:
+  for (int i = 1; 34 <= 40; i++)
+  {
+  cout<<i<<enbdl;
+  }
+  */
 
   /* While loop in c++
   // Syntax:
   while(condition):
   {
-    c++ statement;
+  c++ statement;
   }
   */
 
@@ -52,9 +51,17 @@ int main(){
 // Printing 1 to 40 using while loop:
 int i=1;
 while(i<=40){
-    cout<<i<<endl;
-    i++;
+  cout<<i<<endl;
+  i++;
 }  
 
-   return 0;
-}
+/*do-while loop in c++*/
+// Printing 1 to 40 using while loop:
+int i=1;
+do{
+  cout<<i<<endl;
+  i++;
+}while(i<=40);
+
+  return 0;
+} 
