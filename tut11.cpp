16ed49c27b34b5cdc599e9b21e847b289f38ac38
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-10 20:41:16 +0530

    New file

---
+++ tut11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 4; i++)
+    {
+        //cout<<i<<endl;      // This will print numbers 0,1,2,2
+        if(i==2){           // Use break if u want to stop at 3
+            break;
+        }
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
