bbb34d5aa20975e67c476888ebf929e74d1acae8
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-13 20:54:11 +0530

    New file

--- tut11.cpp
+++ tut11.cpp
@@ -5,7 +5,7 @@ int main(){
     for (int i = 0; i < 4; i++)
     {
         //cout<<i<<endl;      // This will print numbers 0,1,2,2
-        if(i==2){           // Use break if u want to stop at 3
+        if(i==2){           // Use break if u want to stop at 3.
             break;
         }
         cout<<i<<endl;
