6e494729298aa3de994407e04e0b8c7f0007c3c8
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-14 02:45:11 +0530

    New Files

--- CodeTest.cpp
+++ CodeTest.cpp
@@ -11,5 +11,14 @@ int main() {
     for (int i = 1; i <= 10; i++) {
         cout << "6 x " << i << " = " << 6 * i << endl;
     }
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        if(i==2){          
+            continue;
+        }
+        cout<<i<<endl;
+    }
     return 0;
 }
--- tut11.cpp
+++ tut11.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 int main(){
     for (int i = 0; i < 4; i++)
-    {
-        //cout<<i<<endl;      // This will print numbers 0,1,2,2
-        if(i==2){           // Use break if u want to stop at 3.
+    /*{
+        //cout<<i<<endl;      // This will print numbers 0,1,2,3
+        if(i==2){           // Use break if u want to stop at 3
             break;
         }
-        cout<<i<<endl;
+        cout<<i<<endl;        
+    }*/
+   {
+    for (int i = 0; i < 4; i++)
+    {
+        /* code */
     }
+    
+   }
 
     return 0;
 }
