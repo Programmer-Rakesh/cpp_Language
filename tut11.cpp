c219e229b50f96c87bb024781344777f8c26de32
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-09-14 13:00:35 +0530

    Very Imp topic

--- tut11.cpp
+++ tut11.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main(){
-    for (int i = 0; i < 4; i++)
-    /*{
-        //cout<<i<<endl;      // This will print numbers 0,1,2,3
-        if(i==2){           // Use break if u want to stop at 3
-            break;
+    /*for (int i = 0; i < 4; i++)
+    {
+        //cout<<i<<endl;    // This will print numbers 0,1,2,3
+        if(i==2){           //  This will skip 2
+            break;          // Use break if u want to stop at before 2
         }
         cout<<i<<endl;        
     }*/
-   {
     for (int i = 0; i < 4; i++)
     {
-        /* code */
+        if(i==2){             // This will skip 2
+            continue;         // This will continue even after skipping 2
+        }
+        cout<<i<<endl;        // This will print 0,1,3
     }
     
-   }
-
     return 0;
 }
