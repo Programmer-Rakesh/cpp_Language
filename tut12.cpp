e75da775627987f0edfbf2ab191b3f5c75292552
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-10-02 23:05:17 +0530

    Pointers

--- tut12.cpp
+++ tut12.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 using namespace std;
 
-// What is a pointer  ---->  data type which holds the address of other data types
+// What is a pointer?  ---->  Data type which holds the address of other data types
 int main(){
     int a=3;
     int* b = &a;
-    cout<<"The address of a is :"<<&a<<endl;   //
-    cout<<"The address of a is :"<<b<<endl;    //
-    // & --> (Address of) operator    
-    // * --> Dereference operator
+    b = &a;
+
+    // & --> (Address of) operator
+    cout<<"The address of a is :"<<&a<<endl;   
+    cout<<"The address of a is :"<<b<<endl; 
+
+    // * --> (Value at) Dereference operator
+    cout<<"The value at address b is "<<*b<<endl;   // It will give result "3"
 
     return 0;
 }
