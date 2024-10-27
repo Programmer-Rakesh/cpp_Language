bb3f7c48c2d4b42a258235555ef8e439dea2eac8
Programmer-Rakesh <rakeshaps8866@gmail.com>
2024-10-27 23:25:04 +0530

    Done for today

--- tut13.cpp
+++ tut13.cpp
@@ -37,10 +37,10 @@ int main(){
 
     // Pointers and arrays
     int* p = marks;
-    cout<<"The value of marks[0] is"<< *p;
-    cout<<"The value of marks[1] is"<< *(p+1);
-    cout<<"The value of marks[2] is"<< *(p+2);
-    cout<<"The value of marks[3] is"<< *(p+3);
-    
+    cout<<"The value of marks[0] is :"<< *p<<endl;
+    cout<<"The value of marks[1] is :"<< *(p+1)<<endl;
+    cout<<"The value of marks[2] is :"<< *(p+2)<<endl;
+    cout<<"The value of marks[3] is :"<< *(p+3)<<endl;
+
     return 0;
 }
